7320119c7c715d2f4a99014495b1567eeb1f2136
ahmniab <ahmniab11@gmail.com>
2026-04-23 12:59:34 +0200

    reafctor: LeagueController and related DTOs for league management

--- STS.Resources/STS.Resources.Application/DTOs/CreateLeagueDto.cs
+++
@@ -1,8 +0,0 @@
-namespace STS.Resources.Application.DTOs;
-public class CreateLeagueDto
-{
-    public Guid OwnerId { get; set; }
-    public string Name { get; set; } 
-    public DateTime StartDate { get; set; }
-    public string? LogoUrl { get; set; } 
-}
--- STS.Resources/STS.Resources.Application/DTOs/GetLeaguesDto.cs
+++
@@ -1,6 +0,0 @@
-namespace STS.Resources.Application.DTOs;
-
-public class GetLeaguesDto
-{
-    public Guid OwnerId { get; set; }
-}
