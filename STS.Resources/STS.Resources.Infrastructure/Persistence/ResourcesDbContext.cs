3223fc7bfbb3e02ff5f3424550f4df107155c955
ahmniab <ahmniab11@gmail.com>
2026-04-23 16:55:23 +0200

    feat: Add DbSet for TimeSlot in ResourcesDbContext

--- STS.Resources/STS.Resources.Infrastructure/Persistence/ResourcesDbContext.cs
+++ STS.Resources/STS.Resources.Infrastructure/Persistence/ResourcesDbContext.cs
@@ -8,6 +8,7 @@ public class ResourcesDbContext : DbContext
     public DbSet<League> Leagues => Set<League>();
     public DbSet<Team> Teams => Set<Team>();
     public DbSet<Stadium> Stadiums => Set<Stadium>();
+    public DbSet<TimeSlot> TimeSlots => Set<TimeSlot>();
     public ResourcesDbContext(DbContextOptions<ResourcesDbContext> options)
         : base(options) { }
 }
