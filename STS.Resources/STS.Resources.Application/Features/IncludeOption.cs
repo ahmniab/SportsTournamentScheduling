f992ea98dc80e375b5aa9a83dac2c01db88baf2f
ahmniab <ahmniab11@gmail.com>
2026-04-24 23:01:25 +0300

    feat: Add IncludeOption enum and LeagueIncludeOptions class for flexible inclusion settings

---
+++ STS.Resources/STS.Resources.Application/Features/IncludeOption.cs
@@ -0,0 +1,8 @@
+namespace STS.Resources.Application.Features;
+
+public enum IncludeOption
+{
+    INCLUDE_NOTHING = 0,
+    INCLUDE_ID = 1,
+    INCLUDE_ALL = 2,
+}
---
+++ STS.Resources/STS.Resources.Application/Features/League/LeagueIncludeOptions.cs
@@ -0,0 +1,8 @@
+namespace STS.Resources.Application.Features.League;
+
+public class LeagueIncludeOptions
+{
+    public IncludeOption IncludeTeams { get; set; }
+    public IncludeOption IncludeStadiums { get; set; }
+    public IncludeOption IncludeTimeSlots { get; set; }
+}
